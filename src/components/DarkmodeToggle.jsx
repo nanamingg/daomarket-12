5304e9d81db0bb1f2950ad6f3fc6e7a65d85de6f
rootel <ckcksdl135@naver.com>
2024-01-26 18:26:58 +0900

    headerTextColor

--- src/components/DarkmodeToggle.jsx
+++ src/components/DarkmodeToggle.jsx
@@ -6,7 +6,7 @@ const DarkmodeToggle = () => {
   return (
     <>
       <button
-        className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 transform hover:scale-110 transition duration-300"
+        className="inline-flex items-center dark:text-gray-600 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 transform hover:scale-110 transition duration-300"
         type="button"
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ import Metamask from "../images/Metamask.png";
 const Header = () => {
   return (
     <>
-      <header className="text-gray-600 body-font">
+      <header className="text-gray-600 body-font dark:text-white">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 dark:text-white">
             (로고)<span className="ml-3 text-xl">Dao Market</span>
           </div>
           <div className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
