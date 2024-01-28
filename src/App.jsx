f7b4daaf0db6236ceef22be14d33381f9410389d
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-01-28 18:51:18 +0900

    Changed Contact Images

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,3 @@ export default App;
 
 // warning이 여러개 뜬다면 최상위 위치에 .env 생성 후 아래 코드 주석 풀고 입력하기
 // GENERATE_SOURCEMAP=false
-
-// 대략 진행 방향은 페이지(Home, About Us, Contact Us 등등)마다 작동되게 하는 Layout(페이지가 이동되고 Header와 Footer가 고정되는)을 만들고 페이지들 안에 components들을 넣어서 구성하면 될 거 같습니다.
-
-// warning이 여러개 뜬다면 최상위 위치에 .env 생성 후 아래 코드 주석 풀고 입력하기
-// GENERATE_SOURCEMAP=false
