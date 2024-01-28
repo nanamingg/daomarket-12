1cc8f9462ccc5175a754a34b19ebf80800b50f33
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-01-29 01:30:19 +0900

    Add WhatIsDaoMarket

---
+++ src/components/WhatIsDaoMarket.jsx
@@ -0,0 +1,27 @@
+const WhatIsDaoMarket = () => {
+  return (
+    <>
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center mt-20">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-5xl text-4xl mb-4 font-medium text-black">
+            Dao Market이란?
+          </h1>
+          <p className="mb-8 leading-relaxed text-black">
+            Dao Market이란 블라블라블라블라블라블라
+            <br />
+            블라블라블라블라블라블라블라블라
+          </p>
+          <div className="flex justify-center text-black">
+            즉, Dao Market은 블라블라블라블라블라블라블라블라블라
+            <br /> 블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <div>여기엔 이미지를 첨부</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WhatIsDaoMarket;
