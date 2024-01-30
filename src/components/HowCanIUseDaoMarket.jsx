c014aea047bc7e1557fc80452ccdeb7cee90116b
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-01-31 01:40:26 +0900

    Add HowCanIUseDaoMarket

---
+++ src/components/HowCanIUseDaoMarket.jsx
@@ -0,0 +1,96 @@
+const HowCanIUseDaoMarket = () => {
+  return (
+    <>
+      <h1 class="sm:text-5xl text-3xl font-medium title-font text-gray-900 mt-32 mb-4">
+        Dao Market은 어떻게 사용하나요?
+      </h1>
+      <p class="text-2xl leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500 text-center">
+        아래 설명을 따라한다면 옆집 강아지 뽀삐도 Dao Market을 사용할 수
+        있습니다.
+      </p>
+      <div class="flex mt-6 justify-center">
+        <div class="w-16 h-1 rounded-full bg-gray-500 inline-flex"></div>
+      </div>
+      <div class="container px-5 py-24 mx-auto flex flex-wrap">
+        <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-white text-bla border border-black border-2 relative z-10 title-font font-medium text-sm">
+            1
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 inline-flex items-center justify-center">
+              이미지 완
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                MetaMask를 회원가입하고 로그인합니다.
+              </h2>
+              <p class="leading-relaxed">부가설명</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-white text-black border border-black border-2 relative z-10 title-font font-medium text-sm">
+            2
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 inline-flex items-center justify-center">
+              이미지 뚜
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                원하는 Dao에 참여를 하거나 제안을 합니다.
+              </h2>
+              <p class="leading-relaxed">부가설명</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-white text-black border border-black border-2 relative z-10 title-font font-medium text-sm">
+            3
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 inline-flex items-center justify-center">
+              이미지 뜨리
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                참여한 Dao에 대한 지분을 NFT로 발행받습니다.
+              </h2>
+              <p class="leading-relaxed">부가설명</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+          <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+          </div>
+          <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-white text-black border border-black border-2 relative z-10 title-font font-medium text-sm">
+            4
+          </div>
+          <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+            <div class="flex-shrink-0 w-24 h-24 inline-flex items-center justify-center">
+              이미지 뽀
+            </div>
+            <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+              <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">
+                Dao NFT Marketplace에서 지분을 거래합니다.
+              </h2>
+              <p class="leading-relaxed">부가설명</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HowCanIUseDaoMarket;
--- src/pages/IntroduceDaoMarket.jsx
+++ src/pages/IntroduceDaoMarket.jsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import WhatIsDaoMarket from "../components/WhatIsDaoMarket";
 import WhyChooseDaoMarket from "../components/WhyChooseDaoMarket";
+import HowCanIUseDaoMarket from "../components/HowCanIUseDaoMarket";
 
 const IntroduceDaoMarket = () => {
   return (
@@ -9,6 +10,7 @@ const IntroduceDaoMarket = () => {
         <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
           <WhatIsDaoMarket />
           <WhyChooseDaoMarket />
+          <HowCanIUseDaoMarket />
         </section>
       </Layout>
     </div>
