ed5aea0883a53134c7d078d520408806465d9717
rootel <ckcksdl135@naver.com>
2024-02-06 12:19:37 +0900

    asd

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -158,7 +158,7 @@ const Agenda = () => {
           </div>
 
           <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
-            제출
+            제출123
           </button>
         </section>
       </Layout>
