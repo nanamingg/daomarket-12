c41bac36ee7fdc135fe61d88a9c951cd66051031
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-02-07 14:58:13 +0900

    Fixed UI

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -153,7 +153,8 @@ const Agenda = () => {
                   type="text"
                   id="description"
                   name="description"
-                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-12 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  placeholder="안건 내용을 입력해 주세요."
                   onChange={(e) => setDescription(e.target.value)}
                 />
               </div>
